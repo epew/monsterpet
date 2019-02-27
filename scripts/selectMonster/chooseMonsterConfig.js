a69a5697f0c948b161ea3b5e6b24741a0e0ad68d
Tivlea Madalina <madalina.tivlea@wirtek.com>
2019-02-27 16:33:55 +0200

    magic and special monsters

--- scripts/selectMonster/chooseMonsterConfig.js
+++ scripts/selectMonster/chooseMonsterConfig.js
@@ -1,17 +1,57 @@
-let monster = {
-	talk
+let basicMonster = function(id, imageLink, isSelected) {
+	this.id = id;
+	this.imageLink = imageLink;
+	this.isSelected = isSelected;
 }
 
-let characterMonster = {
-	sound: 'meow'
+let magicMonsetr = function(magic) {
+	this.magic = magic;
 }
 
-Object.setPrototypeOf(characterMonster, monster);
-// Object.setPrototypeOf(characterMonster, monsters);
-
-characterMonster.talk();
-
-function talk() {
-	console.log(this);
-	console.log(this.sound);
+let specialMonster = function(power, sound){
+	this.power = power;
+	this.sound = sound;
 }
+
+
+Object.setPrototypeOf(magicMonsetr, basicMonster);
+Object.setPrototypeOf(specialMonster, basicMonster);
+
+
+console.dir(magicMonsetr);
+console.dir(specialMonster);
+
+
+let magicMonsters =[];
+let specialMonsters =[];
+
+
+for(let i=0; i<monsters.length; i++) {
+
+	let n = monsters.length/2;
+
+	if(i<n) {
+		monster = new magicMonsetr("alohomora");
+
+		monster.id = monsters[i].id;
+		monster.imageLink = monsters[i].imageLink;
+		monster.isSelected = monsters[i].isSelected;
+
+		magicMonsters.push(monster);
+
+	} else {
+
+		monster = new specialMonster("fly", "rwar");
+
+		monster.id = monsters[i].id;
+		monster.imageLink = monsters[i].imageLink;
+		monster.isSelected = monsters[i].isSelected;
+
+		specialMonsters.push(monster);
+	}
+}
+
+
+console.log(' magicMonsters ', magicMonsters);
+console.log(' specialMonsters ', specialMonsters);
+
